5af53f2362424f0a7aed7c3388c10dd4d6a8f498
lariunkari@gmail.com <lariunkari@gmail.com>
2020-08-11 12:00:54 +0300

    Implemented Pawn orientation to the path and updated the model. Implemented Surge asset package from Pixelplacement for better spline paths.

--- TowerDefense/Assets/Scripts/Entities/Pawns/PawnMovement.cs
+++ TowerDefense/Assets/Scripts/Entities/Pawns/PawnMovement.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DebugUtilities;
+using Pixelplacement;
 
 public class PawnMovement : MonoBehaviour, IEntityLinkable
 {
     protected IEntity m_linkedEntity;
 
-    public Spline2DComponent m_pathSpline;
+    public Spline m_pathSpline;
+    protected float m_pathLength;
+    //public Spline2DComponent m_pathSpline;
+    
     public float m_moveSpeed = 1f;
     protected float m_pathDistanceMoved;
+    protected float m_pathLengthMoved;
+
+    public float m_lookAheadDistance = 0.1f;
+    protected float m_pathLookAtDistance;
+    protected float m_pathLookAtLength;
 
     public void LinkToEntity(IEntity entity)
     {
@@ -31,12 +40,17 @@ public class PawnMovement : MonoBehaviour, IEntityLinkable
 
     protected virtual void Start()
     {
-
+        if (m_pathSpline != null)
+        {
+            m_pathSpline.CalculateLength();
+            m_pathLength = m_pathSpline.Length;
+            DBGLogger.Log(string.Format("Path distance set to {0:F3}", m_pathLength), this, this);
+        }
     }
 
     protected virtual void Update()
     {
-        if (m_pathSpline && m_moveSpeed > 0f)
+        if (m_moveSpeed > 0f && m_pathLength > 0f && m_pathLengthMoved < 1f)
         {
             MoveOnPath();
         }
@@ -46,7 +60,16 @@ public class PawnMovement : MonoBehaviour, IEntityLinkable
     {
         float movement = m_moveSpeed * Time.deltaTime;
         m_pathDistanceMoved += movement;
-        transform.position = m_pathSpline.InterpolateDistanceWorldSpace(m_pathDistanceMoved);
+        m_pathLengthMoved = m_pathDistanceMoved / m_pathLength;
+        transform.position = m_pathSpline.GetPosition(m_pathLengthMoved, true);
+
+        if (m_pathLengthMoved < 1f - m_lookAheadDistance)
+        {
+            m_pathLookAtDistance = m_pathDistanceMoved + m_lookAheadDistance;
+            m_pathLookAtLength = m_pathLookAtDistance / m_pathLength;
+            transform.LookAt(m_pathSpline.GetPosition(m_pathLookAtLength, true), Vector3.up);
+        }
+
         //DBGLogger.Log(string.Format("Moved {0:F2} to position {1:F2} units along path {2}",
         //    movement, m_pathDistanceMoved, m_pathSpline.name), this, this);
     }
