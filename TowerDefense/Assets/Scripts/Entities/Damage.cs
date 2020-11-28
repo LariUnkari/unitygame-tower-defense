69320abe3a2e9aec7d54e839f43db844028f47d5
Lari Unkari <lariunkari@gmail.com>
2020-11-28 02:26:44 +0200

    Added damage struct for playloads and layers for different entity types

---
+++ TowerDefense/Assets/Scripts/Entities/Damage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entities
+{
+    [System.Serializable]
+    public struct Damage
+    {
+        public int amount;
+        public IEntity source;
+        public IEntity instigator;
+
+        public Damage(int amount, IEntity source, IEntity instigator)
+        {
+            this.amount = amount;
+            this.source = source;
+            this.instigator = instigator;
+        }
+    }
+}
